a330fd957759902409a00d7b1bbf0f05ea85c987
Ghenrii <148689562+Ghenrii@users.noreply.github.com>
2024-02-27 14:31:31 +0200

    second commit

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -12,10 +12,43 @@
             <div class="card text-bg-success mb-4">
                 <div class="card-body">
                     <h5 class="card-title">Rezultatas</h5>
-                    <p>Jūsų KMI yra: {{ $bmi }}</p>
+                    <p>Jūsų KMI yra: {{ $bmi }} (
+                    @if ($bmi < 18.5)
+                        Per mažas svoris
+                    @elseif ($bmi >= 18.5 && $bmi < 24.9)
+                        Normalus svoris
+                    @elseif ($bmi >= 25 && $bmi < 29.9)
+                        Viršsvoris
+                    @else
+                        Nutukimas
+                    @endif
+                    )
+                    </p>
                     <a href="{{ route('skaiciuokle') }}" class="btn btn-light">Grįžti į skaičiuoklę</a>
                 </div>
             </div>
+        </div>
+        <div class="col-12">
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <th scope="row">Per mažas svoris</th>
+                        <td>< 18.5</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Normalus svoris</th>
+                        <td>18.5-24.9</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Viršsvoris</th>
+                        <td>25-29.9</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Nutukimas</th>
+                        <td>&#8805 30</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </body>
